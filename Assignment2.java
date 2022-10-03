06b0e9720900287ba8394fd219507fdf215a92e0
fareesaa <97422732+fareesaa@users.noreply.github.com>
2022-10-03 08:19:41 +0700

    Update Beautify code

--- Assignment2.java
+++ Assignment2.java
@@ -5,19 +5,23 @@ public class Assignment1_3{
      public static void main(String []args){
         Scanner input = new Scanner(System.in);
         
+        //Memasukkan jumlah bari dann kolom untuk array 2d
         System.out.println("Masukkan jumlah baris: ");
         int rowarray = input.nextInt();
         System.out.println("Masukkan jumlah kolom: ");
         int colarray = input.nextInt();
         
+        //numbers var untuk data array yang akan menampung berapapun inputan pada 'rowarray' dan 'colarray' untuk menjadikan array 2d
         int [][] numbers = new int[rowarray][colarray];
+        
+        //Menggunakan perulangan untuk menambah data pada array perbarisnya
         for( int row = 0; row < rowarray; row++ ){
             for( int col = 0; col < colarray; col++ ){
                 System.out.print("Baris ke-" + row + " Index ke-" + col + ": ");
                 numbers[row][col] = input.nextInt();
             }
         }
-
+        
         System.out.println("Baris array adalah " + rowarray );
         
         System.out.println(" Kolom array adalah " + colarray );
@@ -26,6 +30,7 @@ public class Assignment1_3{
         
         System.out.println("Array multidimensi 2 dimensi");
         
+        //Menggunakan perulangan untuk menampilkan data array dengan tersusun 
         for(int row = 0; row < numbers.length; row++){
             for(int col = 0; col < numbers[row].length; col++){
                 System.out.format("| %s | \t", numbers[row][col]);
